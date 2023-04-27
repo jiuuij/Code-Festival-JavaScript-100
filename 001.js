6bdc8cce56dba7c927f4598d604ba92fe2eeffe8
jiu.kim <109861096+jiuuij@users.noreply.github.com>
2023-04-28 00:39:05 +0900

    001 문제 나의 풀이도 추가

--- 001.js
+++ 001.js
@@ -2,10 +2,23 @@
  * 다음 배열에서 400, 500를 삭제하는 code를 입력하세요.
  * var nums = [100, 200, 300, 400, 500];
  */
+
 var nums = [100, 200, 300, 400, 500];
 
+영상 풀이1
 nums.pop();
 nums.pop();
-
 console.log(nums);
 
+/**나의 풀이2
+console.log(nums.slice(0,3));
+
+/**나의 풀이3
+ * nums.splice(3,5);
+ * console.log(nums);
+ */
+
+/**나의 풀이4
+ * nums.splice(3);
+ * console.log(nums);
+ */

c807340a2817976b3689ff11d7ba4d6e1abf2568
jiu.kim <109861096+jiuuij@users.noreply.github.com>
2023-05-01 18:15:45 +0900

    문제3 풀이 + 답안

---
+++ 003.js
@@ -0,0 +1,24 @@
+/** 문제3 : 변수의 타입
+ * 다음 출력 값으로 올바른 것은?
+ * 
+ * var arr = [100, 200, 300];
+ * console.log(typeof(arr));
+ * 
+ * 1)  undefined
+ * 2)  string
+ * 3)  number
+ * 4)  object
+ */
+
+/**나의 풀이
+ * 
+ * 4) object
+ * 4번을 선택한 이유는 arr의 타입은 참조타입으로 object니까..!
+ */
+
+/**영상풀이
+ * 
+ * 4)  object
+ * 정답은 '4번'입니다. undefined, string, number 는 모두 기본 자료형(primitive type) 입니다.
+ */
+

ceb8e74cd48e36ec818800e230712e711bfef971
jiu.kim <109861096+jiuuij@users.noreply.github.com>
2023-05-12 17:13:16 +0900

    020,021 풀이완료

---
+++ js/021.js
@@ -0,0 +1,17 @@
+# 문제21 : set은 어떻게 만드나요?
+다음 중 set을 만드는 방법으로 올바른 것을 모두 고르시오.
+
+1)  var x = {1, 2, 3, 5, 6, 7};
+2)  var x = {};
+3)  var x = new Set('javascript');
+4)  var x = new Set(range(5));
+5)  var x = new Set();
+
+>>나의 풀이
+3)  var x = new Set('javascript');
+5)  var x = new Set();
+
+>>답안
+3)  var x = new Set('javascript');
+5)  var x = new Set();
+//set이란 중복되지 않는 데이터를 저장하는 데이터 구조입니다.

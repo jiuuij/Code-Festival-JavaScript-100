e676699013f473c508d2031dfce28229348035c9
kimjiu <jiwoo2985@naver.com>
2023-05-28 00:19:40 +0900

    050 문제풀이

--- js/050.js
+++ js/050.js
@@ -27,11 +27,11 @@ function bubble(arr) {
     let result = arr.slice(); 
   
     for (let i = 0; i < result.length - 1; i++) {
-      for (let j = 0; j < result.length - 1 - i; j++) {
-        if (result[j] > result[j+1]) {
-          let temp = result[j];
-          result[j] = result[j+1];
-          result[j+1]=temp;
+      for (let J = 0; J < result.length - 1 - i; J++) {
+        if (result[J] > result[J+1]) {
+          let temp = result[J];
+          result[J] = result[J+1];
+          result[J+1]=temp;
         }
       }
     }
@@ -63,6 +63,29 @@ function bubble(arr) {
   }
   
   const items = prompt('입력해주세요.').split(' ').map((n) => {
+    return parseInt(n, 10);
+  });
+  
+  console.log(bubble(items));
+
+
+  >>영상답안
+  function bubble(arr) {
+    let result = arr.slice(); // 원본 배열 복사
+  
+    for (let i = 0; i < result.length - 1; i++) {
+      for (let j = 0; j < result.length - i - 1; j++) {
+        if (result[j] > result[j + 1]) {
+          let value = result[j];
+          result[j] = result[j+1];
+          result[j+1] = value;
+        }
+      }
+    }
+    return result;
+  }
+  
+  const items = "5 3 2 6 7 9".split(' ').map((n) => {
     return parseInt(n, 10);
   });
   
